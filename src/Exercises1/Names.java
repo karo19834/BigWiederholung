190784f52ebb1a1a3985b6691bc050341572288c
karolinawasalska <karolina1983_4@hotmail.com>
2022-03-25 14:46:23 +0100

    rf

--- src/Exercises1/Names.java
+++ src/Exercises1/Names.java
@@ -2,6 +2,9 @@ package Exercises1;
 
 public class Names {
     public static void main(String[] args) {
-        
+        String vorname = "Karolina ";
+        String nachname = " Wasalska";
+        System.out.println("The full name is " + vorname + nachname);
+
     }
 }
