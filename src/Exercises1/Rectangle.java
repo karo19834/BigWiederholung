1778ed4f07f6dfba928288adbcb3f118867f8eac
karolinawasalska <karolina1983_4@hotmail.com>
2022-03-25 14:48:22 +0100

    g

---
+++ src/Exercises1/Rectangle.java
@@ -0,0 +1,11 @@
+package Exercises1;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        int width= 34;
+        int lenght =32;
+        int circumference= 2*width+2*lenght;
+        int area= width*lenght;
+        System.out.println( " Circumference: "+circumference+ " Area: "+area);
+    }
+}
